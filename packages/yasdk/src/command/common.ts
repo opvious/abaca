fea3ac52a428d2119e33ae3dd91533acadfe42f2
Matthieu Monsch <1216372+mtth@users.noreply.github.com>
2023-04-27 13:39:31 -0700

    Add document output generate option (#32)

--- packages/yasdk/src/command/common.ts
+++ packages/yasdk/src/command/common.ts
@@ -1,7 +1,16 @@
+import {FilePath} from '@opvious/stl-utils/files';
+import {mkdir, writeFile} from 'fs/promises';
 import __inlinable from 'inlinable';
+import path from 'path';
 
 export const packageInfo = __inlinable((ctx) =>
   ctx.enclosing(import.meta.url).metadata()
 );
 
 export const supportedVersions = ['3.0', '3.1'] as const;
+
+/** Writes output to path, creating parent folders as necessary. */
+export async function writeOutput(fp: FilePath, str: string): Promise<void> {
+  await mkdir(path.dirname(fp), {recursive: true});
+  await writeFile(fp, str, 'utf8');
+}
--- packages/yasdk/src/command/resolve.ts
+++ packages/yasdk/src/command/resolve.ts
@@ -1,11 +1,10 @@
 import {ResourceLoader} from '@opvious/stl-utils/files';
 import {Command} from 'commander';
-import {writeFile} from 'fs/promises';
 import path from 'path';
 import YAML from 'yaml';
 import {assertIsOpenapiDocument, loadResolvableResource} from 'yasdk-openapi';
 
-import {supportedVersions} from './common.js';
+import {supportedVersions, writeOutput} from './common.js';
 
 export function resolveCommand(): Command {
   return new Command()
@@ -13,19 +12,20 @@ export function resolveCommand(): Command {
     .alias('r')
     .description('Show fully resolved OpenAPI document')
     .argument('<path>', 'path to OpenAPI document (v3.0 or v3.1)')
-    .option('-s, --skip-validation', 'skip schema validation')
     .option('-o, --output <path>', 'output file path (default: stdin)')
-    .option('-r, --root <path>', 'loader root path (default: CWD)')
+    .option('-s, --skip-validation', 'skip schema validation')
+    .option('-r, --loader-root <path>', 'loader root path (default: CWD)')
     .action(async (pp, opts) => {
       const {resolved} = await loadResolvableResource(path.resolve(pp), {
-        loader: ResourceLoader.create({root: opts.root}),
+        loader: ResourceLoader.create({root: opts.loaderRoot}),
       });
+      const doc = resolved.toJS();
       if (!opts.skipValidation) {
-        assertIsOpenapiDocument(resolved, {versions: supportedVersions});
+        assertIsOpenapiDocument(doc, {versions: supportedVersions});
       }
-      const out = YAML.stringify(resolved);
+      const out = YAML.stringify(doc);
       if (opts.output) {
-        await writeFile(opts.output, out, 'utf8');
+        await writeOutput(opts.output, out);
       } else {
         console.log(out);
       }
