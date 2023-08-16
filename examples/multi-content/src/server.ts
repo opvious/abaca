d4d8f88f2677951cb9ff86b760975069f409ae22
Matthieu Monsch <matthieu.monsch@gmail.com>
2023-08-15 18:08:46 -0700

    Expand multi-content example documentation

--- examples/multi-content/src/server.ts
+++ examples/multi-content/src/server.ts
@@ -12,16 +12,20 @@ import {operations, Schema} from './sdk.gen.js';
 export async function createRouter(): Promise<Router> {
   // Load OpenAPI specification from resources/ folder
   const document = await loadOpenapiDocument();
-
   // Create the router from type-checked operation handlers
   return createOperationsRouter<operations>({
     document,
     handlers: new Handler(),
+    handleInvalidRequests: true,
   });
 }
 
+// Convenience alias for strongly-typed Koa context for each operation
+// (including request bodies, parameters, ...).
 type Contexts = KoaContextsFor<operations>;
 
+// Similarly, convenience alias for acceptable return values for each operation
+// (response body, status codes, ...).
 type Values = KoaValuesFor<operations>;
 
 class Handler implements KoaHandlersFor<operations> {
@@ -30,21 +34,27 @@ class Handler implements KoaHandlersFor<operations> {
   getTable(ctx: Contexts['getTable']): Values['getTable'] {
     const table = this.tables.get(ctx.params.id);
     if (!table) {
-      return 404;
+      return 404; // Type-checked (405 would fail for example).
     }
     if (ctx.accepts(['application/json', 'text/csv']) === 'application/json') {
+      // When the `type` field is omitted, the SDK's default is used
+      // (application/json here) and the `data` type-checked against it.
       return {data: table};
     }
-      return {
-        type: 'text/csv',
-        data: Buffer.from(table.rows?.map((r) => r.join(',')).join('\n') ?? ''),
-      };
-
+    // When a `type` field is set, the `data` field must match exacty that
+    // content-type's expected type (`string` here). Setting `table` as data
+    // directly would fail for example.
+    return {
+      type: 'text/csv',
+      data: Buffer.from(table.rows?.map((r) => r.join(',')).join('\n') ?? ''),
+    };
   }
 
   setTable(ctx: Contexts['setTable']): Values['setTable'] {
     let table: Schema<'Table'>;
     switch (ctx.request.type) {
+      // In each branch below, the type is narrowed automatically to the
+      // corresponding content-type's schema.
       case 'application/json':
         table = ctx.request.body;
         break;
@@ -54,11 +64,11 @@ class Handler implements KoaHandlersFor<operations> {
     const {id} = ctx.params;
     const created = !this.tables.has(id);
     this.tables.set(id, table);
-    return created ? 201 : 204;
+    return created ? 201 : 204; // Type-checked.
   }
 
   createTables(): Values['clearTables'] {
     this.tables.clear();
-    return 204;
+    return 204; // Type-checked.
   }
 }
